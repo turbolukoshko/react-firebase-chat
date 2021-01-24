96a334de0add490e44790744fc4729cca606daf2
turbolukoshko <turbo.lukoshko@gmail.com>
2021-01-24 22:54:01 +0200

    connected routes to the app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
+import { useAuthState } from 'react-firebase-hooks/auth';
 import './App.css';
+import Chat from './components/Chat/Chat';
+import Home from './components/Home/Home';
+import Login from './components/Login/Login';
+import SignUp from './components/SignUp/SignUp';
+import { CHAT_ROUTE, HOME_ROUTE, LOGIN_ROUTE, SIGN_UP_ROUTE } from './routes';
+import { PublicRoute } from './HOC/PublicRoute';
+import { PrivateRoute } from './HOC/PrivateRoute';
+import { auth } from './services/firebase';
 
 function App() {
+
+  const [user] = useAuthState(auth());
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <PublicRoute path={LOGIN_ROUTE} component={Login} isAuth={user} />
+        <PublicRoute path={SIGN_UP_ROUTE} component={SignUp} isAuth={user} />
+        <PrivateRoute path={CHAT_ROUTE} component={Chat} isAuth={user} />
+        <PublicRoute path={HOME_ROUTE} component={Home} isAuth={user} exact />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
