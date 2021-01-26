ba714d34f73c4a0a40e925bc067c7dc5312abe5e
turbolukoshko <turbo.lukoshko@gmail.com>
2021-01-26 23:06:55 +0200

    created styles and updated markup for the login component

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { signIn } from '../../helpers/auth';
 import { SIGN_UP_ROUTE } from '../../routes';
+import './Login.scss';
 
 const Login = () => {
 
@@ -23,28 +24,37 @@ const Login = () => {
 
   return(
     <div className="login">
-      <form onSubmit={registerUser}>
+      <form onSubmit={registerUser} className="login__form">
+          <h2 className="login__form-title">Login</h2>
         <div className="login__email">
+          <label className="login__form-label" htmlFor="email">Email</label>
           <input 
             type="email"
             onChange={e => setEmail(e.target.value)}
             value={email}
             placeholder={'Enter your email'}
             name="email"
+            className="login__form-input"
+            id="email"
           />
         </div>
         <div className="login__password">
+        <label className="login__form-label" htmlFor="password">Password</label>
           <input 
             type="password"
             onChange={e => setPassword(e.target.value)}
             value={password}
             placeholder={'Enter your password'}
             name="password"
+            className="login__form-input"
+            id="password"
           />
         </div>
         {error && <p className="login__error">{error}</p>}
-        <button>Sign In</button>
-        <p>Don't have an account? <Link to={SIGN_UP_ROUTE}>Sign Up</Link></p>
+        <div className="login__button-wrapper">
+          <button className="login__btn">Sign In</button>
+        </div>
+        <p className="login__sign-up">Don't have an account? <Link to={SIGN_UP_ROUTE} className="login__sign-up-link">Sign Up</Link></p>
       </form>
     </div>
   );
