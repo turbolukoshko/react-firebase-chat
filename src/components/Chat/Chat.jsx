bc20bd2b062f5eb62ad046229d9dfaa2d1481db8
turbolukoshko <turbo.lukoshko@gmail.com>
2021-01-28 00:17:29 +0200

    updated logic for chat component and created styles

--- src/components/Chat/Chat.jsx
+++ src/components/Chat/Chat.jsx
@@ -4,6 +4,7 @@ import { useAuthState } from 'react-firebase-hooks/auth';
 import { auth, firestore } from '../../services/firebase';
 import { useCollectionData } from 'react-firebase-hooks/firestore';
 import Loader from '../Loader/Loader';
+import './Chat.scss';
 
 const Chat = () => {
 
@@ -29,16 +30,50 @@ const Chat = () => {
   }
 
   return(
-    <div>
-      <button onClick={() => auth().signOut() }>Sign Out</button>
+    <div className="chat">
+      <nav className="chat__navigation-wrapper">
+        <div className="chat__navigation">
+          <img 
+            src={user.photoURL ? user.photoURL : '/images/user.png'} 
+            alt="chat__navigation-avatar" 
+            className="chat__navigation-avatar"
+          />
+          <p className="chat__navigation-username">
+            {user.email ? user.email : user.displayName}
+          </p>
+          <button 
+            onClick={() => auth().signOut() }
+            className="chat__navigation-btn"
+          >
+            Sign Out
+          </button>
+        </div>
+      </nav>
       <div className="chat__field">
-        <div className="message">
+        <div className="chat__field-messages">
           {messages.map(message => {
-            return(<div>
-              <img src={message.photoURL} alt={message.photoURL} />
-              <p>{message.email}</p>
-              <p>{message.text}</p>
-            </div>)
+            return(
+              <div
+                key={message.createdAt}
+                className={message.displayName || message.email ===  (user.email || user.displayName) ? 'message-wrapper right' : 'message-wrapper"'}
+              >
+                <div
+                  className="chat__field-message"
+                >
+                  <img 
+                    src={message.photoURL ? message.photoURL : '/images/user.png'} alt={message.photoURL}
+                    alt="avatar"
+                    className="chat__field-avatar"
+                  />
+                  <div className="chat__field-content">
+                    <p className="chat__field-content-username">
+                      {message.displayName ? message.displayName : message.email}
+                    </p>
+                    <p className="chat__field-content-message" >{message.text}</p>
+                  </div>
+                </div>
+              </div>
+            )
           })}
         </div>
       </div>
