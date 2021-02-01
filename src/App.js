d4e330b80eaf1d7b0d3f03f6a490fbaa044ca3d6
turbolukoshko <turbo.lukoshko@gmail.com>
2021-02-01 23:39:34 +0200

    fixed App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
+import { BrowserRouter, Switch } from 'react-router-dom';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import './App.css';
 import Chat from './components/Chat/Chat';
